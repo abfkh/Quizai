e3f9b62f39fa8bc95eaa31234f88bbc8c8a4368b
abfkh <84881348+abfkh@users.noreply.github.com>
2023-08-29 04:16:31 +0530

    commit 3

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -1,9 +1,162 @@
+// "use client";
+// import React from "react";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "./ui/card";
+// import { Form } from "react-hook-form";
+// import {
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "./ui/form";
+// import { Button } from "./ui/button";
+// import { useForm } from "react-hook-form";
+// import { quizCreationSchema } from "@/schemas/form/quiz";
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { Input } from "./ui/input";
+"use client";
+// import { quizCreationSchema } from "@/schemas/forms/quiz";
 import React from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { BookOpen, CopyCheck } from "lucide-react";
 
-type Props = {};
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoadingQuestions from "../LoadingQuestions";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { Separator } from "./ui/separator";
 
+type Props = {
+  topic: string;
+};
+
+type Input = z.infer<typeof quizCreationSchema>;
 const QuizCreation = (props: Props) => {
-  return <div>hi</div>;
+  const form = useForm<Input>({
+    resolver: zodResolver(quizCreationSchema),
+    defaultValues: {
+      amount: 3,
+      topic: "",
+      type: "open_ended",
+    },
+  });
+
+  function onSubmit(input: Input) {
+    alert(JSON.stringify(input, null, 2));
+  }
+  form.watch();
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Quiz Creation</CardTitle>
+          <CardDescription>Choose a topic</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="number"
+                        min={1}
+                        max={10}
+                        placeholder="shadcn"
+                        onChange={(e) => {
+                          form.setValue("amount", parseInt(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button
+                  type="button"
+                  onClick={() => {
+                    form.setValue("type", "mcq");
+                  }}
+                  variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                  }
+                >
+                  <CopyCheck className="h-5 w-5 mr-2" />
+                  Multiple choice
+                </Button>
+                <Separator className="mr-2 ml-2" orientation="vertical" />
+                <Button
+                  type="button"
+                  onClick={() => {
+                    form.setValue("type", "open_ended");
+                  }}
+                  variant={
+                    form.getValues("type") === "open_ended"
+                      ? "default"
+                      : "secondary"
+                  }
+                >
+                  <BookOpen className="w-5 h-5 mr-2" />
+                  Open Ended
+                </Button>
+              </div>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default QuizCreation;
